ac0d51310ec96387c29d3d6bcbabbc8822dee4dc
Abhishek Sisodiya <31343468+sisodiya2421@users.noreply.github.com>
2020-09-10 14:15:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,13 @@
 # will throw error if variable is unset
 set -eu
 # curl command to ping the url specified in github secrets
-curl -i --head $URL
+curl -w @- -o /dev/null -s "$URL" <<'EOF'
+    time_namelookup:  %{time_namelookup}\n
+       time_connect:  %{time_connect}\n
+    time_appconnect:  %{time_appconnect}\n
+   time_pretransfer:  %{time_pretransfer}\n
+      time_redirect:  %{time_redirect}\n
+ time_starttransfer:  %{time_starttransfer}\n
+                    ----------\n
+         time_total:  %{time_total}\n
+EOF
